1e1aabde7404fdbb6e045aadd014d7a1d28cf25f
Yura_Kirpichnikov <Georgestallker@gmail.com>
2026-07-27 01:02:51 +0300

    test

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,41 @@
-const getUserNames = users => {
-  return users.map(user => user.name);
-};
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@adex.com",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@ux.com",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@wide.com",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@xyz.com",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@abc.com",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@test.com",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@mail.com",
+    balance: 2764,
+  },
+];
+
+const getUserNames = users => users.map(user => user.name);
+
 console.log(getUserNames(users));
